35e464897dc304aeef1193975946f3451ca5a4bb
Dheeraj <dheerajmalvi2004@gmail.com>
2026-07-10 20:10:47 +0530

    Solved env variable problem.

--- commerce/settings.py
+++ commerce/settings.py
@@ -91,11 +91,11 @@ AUTH_USER_MODEL = 'accounts.Account'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mydb',
-        'USER': 'myuser',
-        'PASSWORD': 'mypassword',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        "NAME": config("DB_NAME"),
+        "USER": config("DB_USER"),
+        "PASSWORD": config("DB_PASSWORD"),
+        "HOST": config("DB_HOST"),
+        "PORT": config("DB_PORT"),
     }
 }
 
